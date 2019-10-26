318a7c168ac10c7d81e5a0998d4aa76cf8f50e72
Muhammad Randi <rooholf@gmail.com>
2019-10-26 10:08:17 +0700

    jawaban no 5

---
+++ 5.php
@@ -0,0 +1,22 @@
+<?php
+function setSerial(){
+  $tokens = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  $serial = '';
+  for ($i = 0; $i < 4; $i++){
+      for($j = 0; $j < 4; $j++){
+	        $serial .= $tokens[rand (0, 35)];
+	        }
+      if($i < 3){
+	      $serial .= '-';
+	    }
+    }
+  return $serial;
+}
+
+function generate($x){
+    for ($y = 0; $y < $x; $y++){
+        echo setSerial()."\n";
+    }
+}
+echo generate (5);
+?>
